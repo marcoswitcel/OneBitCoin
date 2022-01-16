ac517dd6001ec9c1df6d2a264e8e163523f46a78
João Marcos <joao@agenciagh.com.br>
2022-01-16 15:36:34 -0300

    lidando com datas

--- App.js
+++ App.js
@@ -1,9 +1,36 @@
 import React from "react";
-import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
 import CurrentPrice from "./src/components/CurrentPrice";
 import HistoryGraphic from "./src/components/HistoryGraphic";
 import QuotationsList from "./src/components/QuotationsList";
 import QuotationsItems from "./src/components/QuotationsList/QuotationsItems";
+
+/**
+ * Faz com que números
+ * @param {number} number 
+ * @param {number} [length]
+ * @returns {string}
+ */
+function addZero(number, length = 2) {
+  return String(number).padStart(length, "0");
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns {string}
+ */
+function formatDate(date) {
+  return `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+}
+
+function url(qtdDay) {
+  const date = new Date; // inicia com a data de hoje
+  const endDate = formatDate(date); // a data final da consulta é hoje
+  date.setDate(date.getDate() - qtdDay); // seta a data do objeto `Date` como send igual a hoje - `qtdDay`.
+  const startDate = formatDate(date); // Gera a data de inicio
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
+}
 
 export default function App() {
   return (
