ade4204db101db277fa0bcdcc17fb431798fcf1e
João Marcos <joao@agenciagh.com.br>
2022-01-15 21:37:38 -0300

    estilo do item da lista 100%

--- src/components/QuotationsList/QuotationsItems/styles.js
+++ src/components/QuotationsList/QuotationsItems/styles.js
@@ -4,7 +4,7 @@ export default StyleSheet.create({
     mainContent: {
         width: "95%",
         height: "auto",
-        backgroundColor: "#ffffff",
+        backgroundColor: "#000000",
         marginLeft: "3%",
         marginBottom: 15,
         borderRadius: 10,
@@ -12,11 +12,33 @@ export default StyleSheet.create({
         alignItems: "center",
         padding: 10,
     },
-    contextLeft: {},
-    contextRight: {},
-    boxLogo: {},
-    logBitcoin: {},
-    dayCotation: {},
-    price: {},
+    contextLeft: {
+        width: "36%",
+        alignItems: "flex-start",
+    },
+    contextRight: {
+        width: "60%",
+        alignItems: "flex-end",
+    },
+    boxLogo: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    logBitcoin: {
+        width: 40,
+        height: 40,
+        marginLeft: 2,
+    },
+    dayCotation: {
+        fontSize: 16,
+        paddingLeft: 2,
+        color: "white",
+        fontWeight: "bold",
+    },
+    price: {
+        fontSize: 19,
+        color: "white",
+        fontWeight: "bold",
+    },
 
 });
